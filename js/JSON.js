f5ec3dc7a9a26dbf06eba7eb81827c2f29c32e82
b79789 <bstacks78@fullsail.edu>
2013-09-10 16:34:45 -0400

    added JSON file added data.xml file recoded my form not to submit twice

---
+++ js/JSON.js
@@ -0,0 +1,23 @@
+var info = {
+	"estimate1": {
+		"formFirstName": ["First Name","Brian"],
+		"formLastName": ["Last Name","Smith"],
+		"formEmail": ["Email" ,"test@me.com"],
+		"formPhone": ["Phone" ,"999-999-9999"]
+	},
+	"estimate2": {
+		"formFirstName": ["First Name","Jonny"],
+		"formLastName": ["Last Name","Wane"],
+		"formEmail": ["Email" ,"retest@me.com"],
+		"formPhone": ["Phone" ,"111-222-3333"]
+	},
+	"estimate3": {
+		"formFirstName": ["First Name","Nikki"],
+		"formLastName": ["Last Name","Well"],
+		"formEmail": ["Email" ,"mytest@me.com"],
+		"formPhone": ["Phone" ,"000-000-5555"]
+	}
+
+	
+	
+}
---
+++ js/Untitled.php
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+
+<items>
+    <item>
+        <name1>Jon</name1>
+        <name2>Smith</name2>
+        <email>test@me.com</email>
+        <phone>999-999-9999</phone>
+    </item>
+</items> 
