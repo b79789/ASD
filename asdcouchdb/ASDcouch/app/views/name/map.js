f1e6c2d31a99b2ab97601941f36bb891c948da8d
b79789 <bstacks78@fullsail.edu>
2013-09-26 22:31:49 -0400

    trying to complete my new app

--- asdcouchdb/ASDcouch/app/views/name/map.js
+++ asdcouchdb/ASDcouch/app/views/name/map.js
@@ -1,11 +1,5 @@
 function(doc) {
-  if (doc._id.substr(0,9) === "Estimate1") {
-    emit(doc._id.substr(8),{
-    	"formFirstName": doc.formFirstName,
-    	"formLastName": doc.formLastName,
-    	"formEmail": doc.formEmail,
-    	"formPhone": doc.formPhone
-    
-    });
+  if (doc._id.substr(0,8) === "Estimate") {
+    emit(doc._id);
   }
 };
